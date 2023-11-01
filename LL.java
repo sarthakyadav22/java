d2c06af9cbc5271587e98888472284f6ea131033
SARTHAK <144370369+sarthakyadav22@users.noreply.github.com>
2023-11-01 18:20:15 +0530

    Create LL.java

---
+++ LL.java
@@ -0,0 +1,25 @@
+package ggg;
+import java.util.*;
+
+public class LL {
+ public static void main(String args[]) {
+	 LinkedList<String>list = new LinkedList<>();
+	 list.add("me");
+	 list.add("you");
+	 list.add("myself");
+	 list.add("puchak");
+	 list.add("element");
+	 list.add("patt");
+	 list.add("nothing");
+	 list.add("goes");
+	 list.add("right");
+	 list.add("go");
+	 list.add("left");
+	 System.out.println(list);
+	 //removing an element from the list
+	 list.remove("me");
+	 list.remove("you");
+	 list.remove("myself");
+	 System.out.println(list);
+ }
+}
