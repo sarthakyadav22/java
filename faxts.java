3441f177cc6c4a27db9cf02793276849c7669b40
sarthakyadav22 <144370369+sarthakyadav22@users.noreply.github.com>
2023-11-01 18:04:01 +0530

    Create faxts.java

---
+++ faxts.java
@@ -0,0 +1,20 @@
+package ggg;
+
+import java.util.*;
+ public class faxts{
+	 public static void main(String args[]) {
+		 ArrayList<String>list = new ArrayList<>();
+		 list.add("faxcts");
+		 list.add("capss");
+		 list.add("Stringssxx");
+		 list.add(1,"SIGMA");
+		 System.out.println("initial list"+list);
+		 list.add("lighter");
+		 list.add("water");
+		 list.add("haayyyy");
+		 list.add("rayta");
+		 System.out.println("current list"+list);
+		 list.remove("haayyyy");
+		 System.out.println("new list"+list);
+	 }
+ }
